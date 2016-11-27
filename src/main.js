07c1fc34caf83ce110d2d86b4f8a98ed58fc5caa
rerum <t@t.t>
2016-11-27 15:50:18 +0300

    class test

--- src/main.js
+++ src/main.js
@@ -6,19 +6,38 @@ co(function*() {
 	var Mongorito = require('mongorito');
 	var Model = Mongorito.Model;
 
-	Mongorito.connect('localhost/DBLab');
+	Mongorito.connect('localhost/test');
 	
 	console.log('test');
 		
 
 	class Account extends Model {	
+		configure () {
+			this.before('save', 'validate');
+		}
+		
+		* validate (next) {
+			console.log(this);
+			
+			yield next;
+		}
+		
 	}
 
 	var account = new Account({
 		name: 'steam_god',
 		username: 'gaben',
-		email: 't@example.com',
+		email: 't@example1.com',
 		phone: "8 800 555 35 35",
+		language: 'english',
+		owned_products: ['Half Life 3']
+	});
+	
+	var account2 = new Account({
+		name: 'steam_god2',
+		username: 'gaben2',
+		email: 't@example2.com',
+		phone: "8 800 555 35 352",
 		language: 'english',
 		owned_products: ['Half Life 3']
 	});
@@ -30,6 +49,7 @@ co(function*() {
 	console.log(accounts);
 	
 	Mongorito.disconnect();
+	process.exit();
 });
 
 
