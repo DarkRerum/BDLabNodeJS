da5af69a95a023f49aac043db8177ea90314e324
rerum <t@t.t>
2016-11-27 14:20:41 +0300

    mongo connection test

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017/DBLab", function(err, db) {
+	if(!err) {
+		console.log("We are connected");
+	}
+});
 console.log('test');
