55085bbea90789586437e4845d27df403face223
Hisozahn <ya@d.com>
2016-12-10 13:29:45 +0300

    Schemas define, test insert, test populate

--- src/main.js
+++ src/main.js
@@ -1,20 +1,93 @@
 var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/test');
 
 var models = require('./models')(mongoose);
 
-mongoose.Promise = global.Promise;
+var Account = models.Accounts;
+var Product = models.Products;
+var Order = models.Orders;
 
-var testAcc = new models.Accounts({
-	name: 'rerum',
+
+var testAcc = new Account({
+	name: 'rerum2',
 	username: 'Acinonyx',
 	email: 't@example.com',
 	phone: "8 800 555 35 35",
-	language: 'english',
-	owned_products: ['Fallout: New Vegas']
+	//language: 'english',
+	//owned_products: ['Fallout: New Vegas']
 });
 
-testAcc.save(function (err) {
-  if (err) return handleError(err);
-  // saved!
+
+var testProd = new Product({
+	name: 'Fallout: New Vegas',
+	price: [
+			{cur: 'rur', value:  400},
+			{cur: 'eur', value: 15},
+			{cur: 'usd', value: 20}
+		],
+		description: [
+			{lang: 'russian', text: 'эммм'},
+			{lang: 'english', text: 'hmmm'}
+			],
+		achievements: [
+			{
+				id: 1,
+				translations: [
+					{lang: 'russian', name: 'старт', text: 'стартани'},
+					{lang: 'english', name: 'start', text: 'start game'}
+				]
+			}
+		]
+});
+/*testProd.save(function (err) {
+	if (err) {
+		console.log(err.errmsg); 
+	}
+});*/
+/*
+Product.findOne({name: 'Fallout: New Vegas'}).exec(function(err,doc) {
+	if (err) {
+	  console.log(err);
+	}
+	else {
+		console.log(0);
+		console.log(doc);
+		testAcc.owned_products = doc._id;
+		console.log(1);
+		console.log(testAcc);
+		testAcc.save(function (err) {
+			if (err) {
+				console.log(err.errmsg);
+			}
+			else {
+				console.log(2);
+			}
+		});
+	}
+});
+*/
+	
+/*Product
+.findOne({name: 'Fallout: New Vegas'})
+.exec(function(err,doc) {
+	if (err) {
+		console.log(err);
+	}
+	else {
+		console.log(doc.achievements[0].translations);
+	}
+});*/
+
+Account
+.findOne({ name: 'rerum2' })
+.populate('owned_products description') // <--
+.exec(function (err, doc) {
+  if (err) {
+	  console.log(err);
+  }
+  else {
+	  //console.log(doc);
+	  console.log('Descriptions are %s', doc.owned_products[0].description);
+  }
 })
--- src/models.js
+++ src/models.js
@@ -6,23 +6,106 @@ module.exports = function(mongoose) {
 	
 	
 	var Account = new Schema({
-		name: String,
+		name: {
+			type: String,
+			required: true,
+			unique: true
+		},
 		username: String,
 		email: String,
 		phone: String,
-		language: String,
-		owned_products: [ String ]
+		language: {
+			type: String,
+			default: 'english'
+		},
+		owned_products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Products' }]
+	});
+	
+	var Product = new Schema({
+		name: {
+			type: String,
+			required: true,
+			unique: true
+		},
+		price: [{cur: String, value:  Number}],
+		description: [{lang: String, text: String}],
+		achievements: [{
+			id: Number,
+			translations: [{
+				lang: String,
+				name: String,
+				text: String
+			}]
+		}]
+	});
+	
+	
+	
+	var Order = new Schema({
+		owner : [{ type: mongoose.Schema.Types.ObjectId, ref: 'Accounts' }],
+		items : [{
+			product : {type: mongoose.Schema.Types.ObjectId, ref: 'Products'},
+			cur: String,
+			value: Number
+		}]
 	});
 	
 	var models = {
-		Accounts : mongoose.model('Accounts', Account)
+		Products : mongoose.model('Products', Product),
+		Accounts : mongoose.model('Accounts', Account),
+		Orders: mongoose.model('Orders', Order)
 	};
 	return models;
-	
-	/*var Product = new Schema({
-		name: String,
-		price: [{cur: String, value: long}],
-		description: [{lang: String, text: String}],
-		achievements: [{id: int, }]
-	})*/;
 }
+/*var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Product = new Schema({
+	name: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	price: [{cur: String, value:  Number}],
+	description: [{lang: String, text: String}],
+	achievements: [{
+		id: Number,
+		translations: [{
+			lang: String,
+			name: String,
+			text: String
+		}]
+	}]
+});
+
+var Account = new Schema({
+	name: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	username: String,
+	email: String,
+	phone: String,
+	language: {
+		type: String,
+		default: 'en'
+	},
+	owned_products: [{ type: String, ref: 'Product' }]
+});
+	
+var Order = new Schema({
+	owner : [{ type: String, ref: 'Account' }],
+	items : [{
+		product : {type: String, ref: 'Product'},
+		cur: String,
+		value: Number
+	}]
+});
+
+module.exports = {
+	Products : mongoose.model('Products', Product),
+	Accounts : mongoose.model('Accounts', Account),
+	Orders: mongoose.model('Orders', Order)
+};
+*/
