bb7e817f79768e8ebc5871a7e0552b8716f6492b
rerum <t@t.t>
2016-12-10 21:28:13 +0300

    new commands

--- src/cli.js
+++ src/cli.js
@@ -12,21 +12,67 @@ var Product = models.Products;
 var Order = models.Orders;
 
 vorpal
-  .command('account ownedproducts <accountname>', "Lists account's owned product names")
+  .command('account add <accountname> <username> <email> <language>', "Creates new account.")
+  .action(function(args, callback) {	
+	
+	var testAcc = new Account({
+		name: args.accountname,
+		username: args.username,
+		email: args.email,
+		language: args.language
+	});
+	
+	testAcc.save(function (err) {
+		if (err) {
+			vorpal.log(err.errmsg); 
+		} else {
+			vorpal.log("Successfully created an account");
+		}
+	});		
+		
+    callback();
+  });
+
+// needs checking
+vorpal
+  .command('account ownedproducts <accountname>', "Lists account's owned product names.")
   .action(function(args, callback) {	
 	
 	functions.getOwnedProducts(models, args.accountname
 	, function(err, data) {
 		if (err) {console.log(err.errmsg)}
 		else {
-			vorpal.log(data);
+			for (var i in data) {
+				vorpal.log(i);
+			}
+		}
+	}
+	);
+		
+    callback();
+  });  
+
+
+vorpal
+  .command('account printdata <accountname>', "Prints info on given account.")
+  .action(function(args, callback) {	
+	
+	functions.getAccountData(models, args.accountname
+	, function(err, data) {
+		if (err) {console.log(err.errmsg)}
+		else {
+			vorpal.log('Name: ' + data.name);
+			vorpal.log('Username: ' + data.username);
+			vorpal.log('Email: ' + data.email);
+			vorpal.log('Language: ' + data.language);
 		}
 	}
 );
 		
     callback();
   });  
-
+  
+  
 vorpal
   .command('product ach <productname> <language>', "Lists product's achievements in specified language")
   .action(function(args, callback) {	
