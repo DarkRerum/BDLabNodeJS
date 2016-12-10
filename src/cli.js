2fea2c63a97b07f7a1cd27ce378999128d4e91dd
rerum <t@t.t>
2016-12-10 18:12:28 +0300

    cli test example

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,30 @@
 const vorpal = require('vorpal')();
 
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/test');
+
+var models = require('./models')(mongoose);
+var functions = require('./functions');
+
+var Account = models.Accounts;
+var Product = models.Products;
+var Order = models.Orders;
+
+
 vorpal
-  .command('foo', 'Outputs "bar".')
-  .action(function(args, callback) {
-    this.log('bar');
+  .command('product price <productname> <currency>', 'Gets product price in provided currency')
+  .action(function(args, callback) {	
+	
+	functions.getProductPrice(models, args.productname, args.currency
+	, function(err, data) {
+		if (err) {console.log(err.errmsg)}
+		else {
+			vorpal.log(data);
+		}
+	}
+);
+		
     callback();
   });
 
